2f632efecea81e6937203d7c6339cf331c4d4c10
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-05-13 07:24:11 +0530

    h2 to postgres

--- src/main/java/com/expensetrace/app/responseDto/UserResponseDto.java
+++ src/main/java/com/expensetrace/app/responseDto/UserResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class UserResponseDto {
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
 }
