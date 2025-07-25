ef0b53b36ee4859e121762210d42c8503deab537
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-07-25 19:47:38 +0530

    Transaction enum type

--- src/main/java/com/expensetrace/app/enums/TransactionType.java
+++ src/main/java/com/expensetrace/app/enums/TransactionType.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "Transaction Type: 1=INCOME, 2=EXPENSE, 3=TRANSFER")
+@Schema(description = "Transaction Type: 1=EXPENSE, 2=INCOME, 3=TRANSFER")
 public enum TransactionType {
-    INCOME(1),
-    EXPENSE(2),
+    EXPENSE(1),
+    INCOME(2),
     TRANSFER(3);
 
     private final int code;
