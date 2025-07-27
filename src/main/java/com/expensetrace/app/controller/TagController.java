77e24919f91e166d60bdb808b6de771e85d1f096
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-07-27 14:01:10 +0530

    tag

--- src/main/java/com/expensetrace/app/controller/TagController.java
+++ src/main/java/com/expensetrace/app/controller/TagController.java
@@ -36,22 +36,40 @@ public class TagController {
         try {
             List<TagResponseDto> tagResponseDto = new ArrayList<>();
             TagResponseDto tagResponse1=new TagResponseDto();
-            tagResponse1.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c57"));
+            tagResponse1.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c11"));
             tagResponse1.setTransactions(4);
             tagResponse1.setName("tag1");
             tagResponseDto.add(tagResponse1);
 
             TagResponseDto tagResponse2=new TagResponseDto();
-            tagResponse2.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c57"));
+            tagResponse2.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c22"));
             tagResponse2.setTransactions(5);
             tagResponse2.setName("tag2");
             tagResponseDto.add(tagResponse2);
 
             TagResponseDto tagResponse3=new TagResponseDto();
-            tagResponse3.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c57"));
+            tagResponse3.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c33"));
             tagResponse3.setTransactions(3);
             tagResponse3.setName("tag3");
             tagResponseDto.add(tagResponse3);
+
+            TagResponseDto tagResponse4=new TagResponseDto();
+            tagResponse4.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c44"));
+            tagResponse4.setTransactions(4);
+            tagResponse4.setName("tag4");
+            tagResponseDto.add(tagResponse4);
+
+            TagResponseDto tagResponse5=new TagResponseDto();
+            tagResponse5.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c55"));
+            tagResponse5.setTransactions(5);
+            tagResponse5.setName("tag5");
+            tagResponseDto.add(tagResponse5);
+
+            TagResponseDto tagResponse6=new TagResponseDto();
+            tagResponse6.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c66"));
+            tagResponse6.setTransactions(3);
+            tagResponse6.setName("tag6");
+            tagResponseDto.add(tagResponse6);
 
             return ResponseEntity.ok(new ApiResponse("Found!", tagResponseDto));
         } catch (Exception e) {
@@ -63,7 +81,7 @@ public class TagController {
             summary = "Delete a tag by ID",
             description = "Deletes the tag with the specified ID. Throws an error if the tag is not found."
     )
-    @DeleteMapping("/tag/{id}/delete")
+    @DeleteMapping("/{id}/delete")
     public ResponseEntity<ApiResponse> deleteTag(@PathVariable UUID id) {
         try {
             //tagService.deleteTagById(id);
@@ -77,7 +95,7 @@ public class TagController {
             summary = "merge a tag by ID",
             description = "Merges the tag with the specified ID into another tag identified by tagId parameter. Throws an error if either tag is not found."
     )
-    @PutMapping("/tag/{id}/merge")
+    @PutMapping("/{id}/merge")
     public ResponseEntity<ApiResponse> mergeTag(@PathVariable UUID id, @RequestParam UUID tagId) {
         try {
             return ResponseEntity.ok(new ApiResponse("Deleted successfully", null));
@@ -90,7 +108,7 @@ public class TagController {
             summary = "Update a tag by ID",
             description = "Updates the tag with the specified ID using the provided details. Throws an error if the tag is not found."
     )
-    @PutMapping("/tag/{id}/update")
+    @PutMapping("/{id}/update")
     public ResponseEntity<ApiResponse> updateTag(@PathVariable UUID id, @RequestBody TagRequestDto tag) {
         try {
             //TagResponseDto updatedTagResponseDto = tagService.updateTag(tag, id);
