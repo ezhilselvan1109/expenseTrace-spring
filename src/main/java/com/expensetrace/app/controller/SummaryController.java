6ddcac9a29eb5fd1e05377a73f2b773cd295a71d
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-07-28 16:43:23 +0530

    sample analysis data added

--- src/main/java/com/expensetrace/app/controller/SummaryController.java
+++ src/main/java/com/expensetrace/app/controller/SummaryController.java
@@ -93,40 +93,44 @@ public class SummaryController {
             List<AccountSummary> spendingAccount = new ArrayList<>();
             List<AccountSummary> transfersAccount = new ArrayList<>();
 
-            AccountSummary accountSummary = new AccountSummary();
-            AccountResponseDto accountResponseDto=new AccountResponseDto();
-            accountResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c66"));
-            accountResponseDto.setName("Bank");
-            accountResponseDto.setType(AccountType.fromCode(1));
-            accountResponseDto.setDefault(false);
-            accountSummary.setAccountResponseDto(accountResponseDto);
-            accountSummary.setAmount(500);
+            AccountSummary BankAccountSummary = new AccountSummary();
+            AccountResponseDto BankAccountResponseDto=new AccountResponseDto();
+            BankAccountResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c66"));
+            BankAccountResponseDto.setName("Bank");
+            BankAccountResponseDto.setType(AccountType.fromCode(1));
+            BankAccountResponseDto.setDefault(false);
+            BankAccountSummary.setAccountResponseDto(BankAccountResponseDto);
+            BankAccountSummary.setAmount(500);
 
-            incomeAccount.add(accountSummary);
-            spendingAccount.add(accountSummary);
-            transfersAccount.add(accountSummary);
+            incomeAccount.add(BankAccountSummary);
+            spendingAccount.add(BankAccountSummary);
+//            transfersAccount.add(BankAccountSummary);
 
-            accountResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c76"));
-            accountResponseDto.setName("Card");
-            accountResponseDto.setType(AccountType.fromCode(2));
-            accountResponseDto.setDefault(false);
-            accountSummary.setAccountResponseDto(accountResponseDto);
-            accountSummary.setAmount(300);
+            AccountSummary CardAccountSummary = new AccountSummary();
+            AccountResponseDto CardAccountResponseDto=new AccountResponseDto();
+            CardAccountResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c76"));
+            CardAccountResponseDto.setName("Card");
+            CardAccountResponseDto.setType(AccountType.fromCode(2));
+            CardAccountResponseDto.setDefault(false);
+            CardAccountSummary.setAccountResponseDto(CardAccountResponseDto);
+            CardAccountSummary.setAmount(300);
 
-            incomeAccount.add(accountSummary);
-            spendingAccount.add(accountSummary);
-            transfersAccount.add(accountSummary);
+            incomeAccount.add(CardAccountSummary);
+            spendingAccount.add(CardAccountSummary);
+//            transfersAccount.add(CardAccountSummary);
 
-            accountResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c86"));
-            accountResponseDto.setName("cash");
-            accountResponseDto.setType(AccountType.fromCode(1));
-            accountResponseDto.setDefault(false);
-            accountSummary.setAccountResponseDto(accountResponseDto);
-            accountSummary.setAmount(600);
+            AccountSummary cashAccountSummary = new AccountSummary();
+            AccountResponseDto cashAccountResponseDto=new AccountResponseDto();
+            cashAccountResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c86"));
+            cashAccountResponseDto.setName("cash");
+            cashAccountResponseDto.setType(AccountType.fromCode(1));
+            cashAccountResponseDto.setDefault(false);
+            cashAccountSummary.setAccountResponseDto(cashAccountResponseDto);
+            cashAccountSummary.setAmount(600);
 
-            incomeAccount.add(accountSummary);
-            spendingAccount.add(accountSummary);
-            transfersAccount.add(accountSummary);
+            incomeAccount.add(cashAccountSummary);
+            spendingAccount.add(cashAccountSummary);
+//            transfersAccount.add(cashAccountSummary);
 
             analysisSummaryResponse.setIncomeAccount(incomeAccount);
             analysisSummaryResponse.setSpendingAccount(spendingAccount);
@@ -136,63 +140,67 @@ public class SummaryController {
             List<CategorySummary> spendingCategory = new ArrayList<>();
             List<CategorySummary> incomeCategory = new ArrayList<>();
 
-            CategorySummary categorySummary = new CategorySummary();
-            CategoryResponseDto categoryResponseDto=new CategoryResponseDto();
+            CategorySummary foodCategorySummary = new CategorySummary();
+            CategoryResponseDto foodCategoryResponseDto=new CategoryResponseDto();
 
-            categoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c86"));
-            categoryResponseDto.setType(CategoryType.fromCode(1));
-            categoryResponseDto.setName("Food");
-            categoryResponseDto.setIcon("apple");
-            categoryResponseDto.setColor("red");
-            categorySummary.setCategory(categoryResponseDto);
-            categorySummary.setAmount(100);
+            foodCategoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c86"));
+            foodCategoryResponseDto.setType(CategoryType.fromCode(1));
+            foodCategoryResponseDto.setName("Food");
+            foodCategoryResponseDto.setIcon("apple");
+            foodCategoryResponseDto.setColor("red");
+            foodCategorySummary.setCategory(foodCategoryResponseDto);
+            foodCategorySummary.setAmount(100);
 
-            spendingCategory.add(categorySummary);
-            incomeCategory.add(categorySummary);
+            spendingCategory.add(foodCategorySummary);
 
-            categoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c88"));
-            categoryResponseDto.setType(CategoryType.fromCode(2));
-            categoryResponseDto.setName("Travel");
-            categoryResponseDto.setIcon("airplane");
-            categoryResponseDto.setColor("blue");
-            categorySummary.setCategory(categoryResponseDto);
-            categorySummary.setAmount(400);
+            CategorySummary TravelCategorySummary = new CategorySummary();
+            CategoryResponseDto TravelCategoryResponseDto=new CategoryResponseDto();
+            TravelCategoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c88"));
+            TravelCategoryResponseDto.setType(CategoryType.fromCode(2));
+            TravelCategoryResponseDto.setName("Travel");
+            TravelCategoryResponseDto.setIcon("airplane");
+            TravelCategoryResponseDto.setColor("blue");
+            TravelCategorySummary.setCategory(TravelCategoryResponseDto);
+            TravelCategorySummary.setAmount(400);
 
-            spendingCategory.add(categorySummary);
-            incomeCategory.add(categorySummary);
+            incomeCategory.add(TravelCategorySummary);
 
-            categoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c89"));
-            categoryResponseDto.setType(CategoryType.fromCode(1));
-            categoryResponseDto.setName("Medical");
-            categoryResponseDto.setIcon("pill");
-            categoryResponseDto.setColor("teal");
-            categorySummary.setCategory(categoryResponseDto);
-            categorySummary.setAmount(700);
+            CategorySummary MedicalCategorySummary = new CategorySummary();
+            CategoryResponseDto MedicalCategoryResponseDto=new CategoryResponseDto();
+            MedicalCategoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd0c6c89"));
+            MedicalCategoryResponseDto.setType(CategoryType.fromCode(1));
+            MedicalCategoryResponseDto.setName("Medical");
+            MedicalCategoryResponseDto.setIcon("pill");
+            MedicalCategoryResponseDto.setColor("teal");
+            MedicalCategorySummary.setCategory(MedicalCategoryResponseDto);
+            MedicalCategorySummary.setAmount(700);
 
-            spendingCategory.add(categorySummary);
-            incomeCategory.add(categorySummary);
+            spendingCategory.add(MedicalCategorySummary);
 
-            categoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-54b8fd0c6c89"));
-            categoryResponseDto.setType(CategoryType.fromCode(1));
-            categoryResponseDto.setName("Shopping");
-            categoryResponseDto.setIcon("shopping-bag");
-            categoryResponseDto.setColor("violet");
-            categorySummary.setCategory(categoryResponseDto);
-            categorySummary.setAmount(700);
+            CategorySummary ShoppingCategorySummary = new CategorySummary();
+            CategoryResponseDto ShoppingCategoryResponseDto=new CategoryResponseDto();
+            ShoppingCategoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-54b8fd0c6c89"));
+            ShoppingCategoryResponseDto.setType(CategoryType.fromCode(1));
+            ShoppingCategoryResponseDto.setName("Shopping");
+            ShoppingCategoryResponseDto.setIcon("shopping-bag");
+            ShoppingCategoryResponseDto.setColor("violet");
+            ShoppingCategorySummary.setCategory(ShoppingCategoryResponseDto);
+            ShoppingCategorySummary.setAmount(700);
 
-            spendingCategory.add(categorySummary);
-            incomeCategory.add(categorySummary);
+            incomeCategory.add(ShoppingCategorySummary);
 
-            categoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd8c6c89"));
-            categoryResponseDto.setType(CategoryType.fromCode(1));
-            categoryResponseDto.setName("Family");
-            categoryResponseDto.setIcon("home");
-            categoryResponseDto.setColor("orange");
-            categorySummary.setCategory(categoryResponseDto);
-            categorySummary.setAmount(700);
+            CategorySummary FamilyCategorySummary = new CategorySummary();
+            CategoryResponseDto FamilyCategoryResponseDto=new CategoryResponseDto();
+            FamilyCategoryResponseDto.setId(UUID.fromString("1e9431a1-adc3-47cb-8645-51b8fd8c6c89"));
+            FamilyCategoryResponseDto.setType(CategoryType.fromCode(1));
+            FamilyCategoryResponseDto.setName("Family");
+            FamilyCategoryResponseDto.setIcon("home");
+            FamilyCategoryResponseDto.setColor("orange");
+            FamilyCategorySummary.setCategory(FamilyCategoryResponseDto);
+            FamilyCategorySummary.setAmount(700);
 
-            spendingCategory.add(categorySummary);
-            incomeCategory.add(categorySummary);
+            spendingCategory.add(FamilyCategorySummary);
+            incomeCategory.add(FamilyCategorySummary);
 
             analysisSummaryResponse.setSpendingCategory(spendingCategory);
             analysisSummaryResponse.setIncomeCategory(incomeCategory);
