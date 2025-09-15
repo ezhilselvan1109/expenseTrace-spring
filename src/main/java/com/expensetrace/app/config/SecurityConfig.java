c422beaa1ad44f837a2b2880a20023b45ab1cccb
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-09-16 00:28:15 +0530

    refactor(security): add frontend for cors

--- src/main/java/com/expensetrace/app/config/SecurityConfig.java
+++ src/main/java/com/expensetrace/app/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://expensetrace.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
